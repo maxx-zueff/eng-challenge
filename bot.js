487aa07e9571ac3ebf0646c50ac4acb08882e96d
Max Zueff <maxx.zueff@gmail.com>
2018-07-26 21:35:08 +0300

    switch accounts

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@
 // Dependencies
 
 const twit = require('twit');
-const config = require('./config/engchallenge1.js');
-const T = new twit(config);
 const fs = require('fs');
 const util = require('util');
+
+let config = require('./config/config.js');
 
 // --------------------------------------------------------
 // Debug
@@ -21,16 +21,77 @@ console.log = function(d) {
 // --------------------------------------------------------
 // Stream
 
-let hash_tags = ['#adulted', '#eal', '#ellchat', '#ESL',
-	'#grammar', '#twinglish', '#englishdiary',
-	'#100DaysOfEnglish']; 
 
-let stream = T.stream('statuses/filter',
-	{ track: hash_tags });
- 
-stream.on('tweet', function (tweet) {
-	T.post('favorites/create', { id: tweet.id_str }, function (err, data, response) {
-		if (err) console.log(err);
-		else console.log('LIKE!');
+let active = function (id, reset) {
+
+
+	var diff = reset - Date.now();
+
+	setTimeout(function(id){
+		
+		for (let el of config) {
+			if (el.id == id) el.active = true;
+		}
+
+	}, diff);
+
+};
+
+let start = function (reset) {
+
+
+	for (let el of config) {
+
+		if(el.selected == true) {
+			el.selected = false;
+			el.active = false;
+			active(el.id, reset);
+		}
+
+		if (el.active == true) {
+			el.selected = true;
+			bot(new twit(el.data));
+			return true;
+		}
+	}
+
+
+};
+
+let bot = function(T) {
+
+
+	let hash_tags = ['#adulted', '#eal', '#ellchat', '#ESL',
+		'#grammar', '#twinglish', '#englishdiary',
+		'#100DaysOfEnglish']; 
+
+	let stream = T.stream('statuses/filter',
+		{ track: hash_tags });
+	 
+	stream.on('tweet', function (tweet) {
+
+		T.post('favorites/create', { id: tweet.id_str }, function (err, data, response) {
+			console.log(data.text);
+			if (err) console.log(err);
+			if (response.statusCode == 429) {
+				
+				console.log("CODE 429");
+				stream.stop();
+				T.get('application/rate_limit_status', {resources:'favorites'}, function (err, data, response){
+
+					let limit = data.resources.favorites['/favorites/list'];
+					let reset = limit.reset;
+					
+					start(reset); // 429 -- rate limit
+				});
+			}
+		});
 	});
-});
+
+	stream.on('error', function(err) {
+		console.log(err);
+	});
+
+};
+
+start();
